54342c77a742b6c65fba3c9819600efdd7158ca7
Richard Tape <richard.tape@ubc.ca>
2014-07-23 16:28:45 -0700

    ADD getExcerptFromPostID() method to allow us to get the excerpt for a post from an ID ADD locateTemplateInPlugin() method to allow us to include template parts from within a plugin

--- includes/class-studiorum-utils.php
+++ includes/class-studiorum-utils.php
@@ -58,4 +58,103 @@
 
 		}/* usersRoleIs() */
 
+
+		/**
+		 * WordPress's native get_the_excert function has to be called from within a loop. This provides a method to fetch
+		 * a post's excerpt outside of the loop, by ID
+		 *
+		 * @since 0.1
+		 *
+		 * @param int $postID The ID of the post
+		 * @param int $excerptLength The length of the excerpt
+		 * @param bool $withP Whether to wrap the excerpt in <p> tags or not
+		 * @return string The post excerpt
+		 */
+
+		public static function getExcerptFromPostID( $postID, $excerptLength = 35, $withP = false )
+		{
+
+			$postObject = get_post( $postID );
+
+			// Gets post_content to be used as a basis for the excerpt
+			$excerpt = $postObject->post_content;
+			
+			// Strips tags and images
+			$excerpt = strip_tags( strip_shortcodes( $excerpt ) );
+			$words = explode( ' ', $excerpt, $excerptLength + 1 );
+
+			if( count( $words ) > $excerptLength ) :
+
+				array_pop( $words );
+				array_push( $words, '…' );
+				$excerpt = implode( ' ', $words );
+
+			endif;
+
+			if( $withP ){
+				$excerpt = '<p>' . $excerpt . '</p>';
+			}
+
+			return $excerpt;
+
+		}/* getExcerptFromPostID() */
+
+
+		/**
+		 * Method to allow including of a template part from within a plugin. Replicates what locate_template() does in
+		 * WordPress core, but allows you to specify the path where to start looking. Falls back to looking in the
+		 * 
+		 *
+		 * @since 0.1
+		 *
+		 * @param string $startPath The path - probably set via a constant - of where to start looking
+		 * @param string|array $templateNames Template file(s) to search for, in order.
+		 * @param bool $load If true the template file will be loaded if it is found.
+		 * @param bool $requireOnce Whether to require_once or require. Default true. Has no effect if $load is false.
+		 * @return string The template filename if one is located.
+		 */
+
+		public static function locateTemplateInPlugin( $startPath, $templateNames, $load = false, $requireOnce = true )
+		{
+
+			$located = '';
+
+			foreach( (array) $templateNames as $templateName )
+			{
+
+				if( !$templateName ){
+					continue;
+				}
+
+				if( file_exists( untrailingslashit( $startPath ) . '/' . $templateName ) )
+				{
+
+					$located =  untrailingslashit( $startPath ) . '/' . $templateName;
+					break;
+
+				}
+				else if( file_exists( untrailingslashit( STYLESHEETPATH ) . '/' . $templateName ) )
+				{
+					$located = untrailingslashit( STYLESHEETPATH ) . '/' . $templateName;
+					break;
+
+				}
+				else if( file_exists( untrailingslashit( TEMPLATEPATH ) . '/' . $templateName ) )
+				{
+
+					$located = untrailingslashit( TEMPLATEPATH ) . '/' . $templateName;
+					break;
+				
+				}
+			
+			}
+
+			if( $load && '' != $located ){
+				load_template( $located, $require_once );
+			}
+
+			return $located;
+
+		}/* locateTemplateInPlugin() */
+
 	}/* class Studiorum_Utils */
