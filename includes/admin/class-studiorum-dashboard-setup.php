9030d50b9ab3647f834fb5bf5b7c65213c32a033
Richard Tape <richard.tape@ubc.ca>
2014-07-30 15:50:58 -0700

    CHANGE remove ability for students to see submissions menu in dashboard

--- includes/admin/class-studiorum-dashboard-setup.php
+++ includes/admin/class-studiorum-dashboard-setup.php
@@ -136,12 +136,13 @@
 				return false;
 			}
 
-			remove_menu_page( 'upload.php' ); 		// Media
-			remove_menu_page( 'media-new.php' ); 		// Media
-			remove_menu_page( 'link-manager.php' ); // Links
-			remove_menu_page( 'edit.php' ); // Posts
-			remove_menu_page( 'edit-comments.php' ); // Comments
-			remove_menu_page( 'tools.php' ); // Tools
+			remove_menu_page( 'upload.php' ); 							// Uploads
+			remove_menu_page( 'media-new.php' ); 						// Media
+			remove_menu_page( 'link-manager.php' ); 					// Links
+			remove_menu_page( 'edit.php' ); 							// Posts
+			remove_menu_page( 'edit-comments.php' ); 					// Comments
+			remove_menu_page( 'tools.php' ); 							// Tools
+			remove_menu_page( 'edit.php?post_type=lectio-submission' ); // Submissions entries
 
 		}/* admin_menu__removeMenuItemsForStudents() */
 
