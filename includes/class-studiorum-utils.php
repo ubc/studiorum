9709b7a14428553a218857e6c0c776021c5bdf9f
Richard Tape <richard@iamfriendly.com>
2014-10-28 15:22:59 -0700

    CHANGE adjust indent level as much of the file was skipped over one tab. Urgh.

--- includes/class-studiorum-utils.php
+++ includes/class-studiorum-utils.php
@@ -189,311 +189,313 @@
 
 
 		/**
-			 * Get a list of all of our available modules
-			 *
-			 *
-			 * @since 0.1
-			 *
-			 * @param null
-			 * @return array An array of arrays of all of our studiorum modules
-			 */
+		 * Get a list of all of our available modules
+		 *
+		 *
+		 * @since 0.1
+		 *
+		 * @param null
+		 * @return array An array of arrays of all of our studiorum modules
+		 */
 
-			public static function getAllModules()
+		public static function getAllModules()
+		{
+
+			$modules = array();
+
+			return apply_filters( 'studiorum_modules', $modules );
+
+		}/* getAllModules() */
+
+
+		/**
+		 * Method to return a list of plugins currently available which are 'studiorum' plugins
+		 *
+		 * @since 0.1
+		 *
+		 * @param null
+		 * @return array list of studiorum plugins
+		 */
+
+		public static function getStudiorumPlugins()
+		{
+
+			if( !function_exists( 'get_plugins' ) ){
+				require_once ABSPATH . 'wp-admin/includes/plugin.php';
+			}
+
+			// Returns a list of all available plugins
+			$allPlugins = get_plugins();
+
+			if( !$allPlugins || !is_array( $allPlugins ) || empty( $allPlugins ) ){
+				return;
+			}
+
+			$output = array();
+
+			foreach( $allPlugins as $pathAndSlug => $pluginData )
+			{
+			
+				$splitPathAndSlug = explode( '/', $pathAndSlug );
+
+				// Search for 'studiorum' in the folder name
+				$folderName = strtolower( $splitPathAndSlug[0] );
+
+				$searchFor = 'studiorum';
+
+				$pos = strpos( $folderName, $searchFor );
+
+				if( $pos === false ){
+					continue;
+				}
+
+				$output[$folderName] = $pluginData;
+
+			}
+
+			return $output;
+
+		}/* getStudiorumPlugins() */
+
+
+		/**
+		 * Get a list of all studiorum plugins that are *active*
+		 *
+		 * @since 0.1
+		 *
+		 * @param null
+		 * @return array of currently activated studiorum plugins
+		 */
+
+		public static function getActiveStudiorumPlugins()
+		{
+
+			$activePlugins = wp_get_active_and_valid_plugins();
+
+			if( !$activePlugins || !is_array( $activePlugins ) || empty( $activePlugins ) ){
+				return;
+			}
+
+			$output = array();
+
+			// Each active plugin file has the plugin dir prepended, we need to strip that
+			$pathToRemove = WP_PLUGIN_DIR . '/';
+
+			foreach( $activePlugins as $key => $path )
+			{
+			
+				$searchFor = 'studiorum';
+
+				$pos = strpos( $path, $searchFor );
+
+				if( $pos === false ){
+					continue;
+				}
+
+				// Remove the path
+				$withoutPath = str_replace( $pathToRemove, '', $path );
+
+				// Now we just need the directory name
+				$splitPathAndSlug = explode( '/', $withoutPath );
+
+				// Search for 'studiorum' in the folder name
+				$folderName = strtolower( $splitPathAndSlug[0] );
+
+				$output[] = $folderName;
+
+			}
+
+			return $output;
+
+		}/* getActiveStudiorumPlugins() */
+
+
+		/**
+		 * Get all active plugins and strip off the path
+		 *
+		 * @since 0.1
+		 *
+		 * @param string $param description
+		 * @return string|int returnDescription
+		 */
+
+		public static function getAllActivePluginsWithoutPluginPath()
+		{
+
+			$activePlugins = wp_get_active_and_valid_plugins();
+
+			if( !$activePlugins || !is_array( $activePlugins ) || empty( $activePlugins ) ){
+				return;
+			}
+
+			$output = array();
+
+			// Each active plugin file has the plugin dir prepended, we need to strip that
+			$pathToRemove = WP_PLUGIN_DIR . '/';
+
+			foreach( $activePlugins as $key => $path )
 			{
 
-				$modules = array();
+				// Remove the path
+				$withoutPath = str_replace( $pathToRemove, '', $path );
 
-				return apply_filters( 'studiorum_modules', $modules );
+				$output[] = $withoutPath;
 
-			}/* getAllModules() */
+			}
 
+			return $output;
 
-			/**
-			 * Method to return a list of plugins currently available which are 'studiorum' plugins
-			 *
-			 * @since 0.1
-			 *
-			 * @param null
-			 * @return array list of studiorum plugins
-			 */
-
-			public static function getStudiorumPlugins()
-			{
-
-				if( !function_exists( 'get_plugins' ) ){
-					require_once ABSPATH . 'wp-admin/includes/plugin.php';
-				}
-
-				// Returns a list of all available plugins
-				$allPlugins = get_plugins();
-
-				if( !$allPlugins || !is_array( $allPlugins ) || empty( $allPlugins ) ){
-					return;
-				}
-
-				$output = array();
-
-				foreach( $allPlugins as $pathAndSlug => $pluginData )
-				{
-				
-					$splitPathAndSlug = explode( '/', $pathAndSlug );
-
-					// Search for 'studiorum' in the folder name
-					$folderName = strtolower( $splitPathAndSlug[0] );
-
-					$searchFor = 'studiorum';
-
-					$pos = strpos( $folderName, $searchFor );
-
-					if( $pos === false ){
-						continue;
-					}
-
-					$output[$folderName] = $pluginData;
-
-				}
-
-				return $output;
-
-			}/* getStudiorumPlugins() */
+		}/* getAllActivePluginsWithoutPluginPath() */
 
 
-			/**
-			 * Get a list of all studiorum plugins that are *active*
-			 *
-			 * @since 0.1
-			 *
-			 * @param null
-			 * @return array of currently activated studiorum plugins
-			 */
+		/**
+		 * Get the plugin 'groups' which are sets of plugins with some metadata about them
+		 *
+		 * @since 0.1
+		 *
+		 * @todo There should be a class stub which sets up each property and then each group should be an extension of that class which then adds a filter to a list. This method should then simply return that filter
+		 * @param null
+		 * @return array An array of plugin groups
+		 */
 
-			public static function getActiveStudiorumPlugins()
-			{
+		public static function getPluignGroups()
+		{
 
-				$activePlugins = wp_get_active_and_valid_plugins();
+			$groups = array(
 
-				if( !$activePlugins || !is_array( $activePlugins ) || empty( $activePlugins ) ){
-					return;
-				}
-
-				$output = array();
-
-				// Each active plugin file has the plugin dir prepended, we need to strip that
-				$pathToRemove = WP_PLUGIN_DIR . '/';
-
-				foreach( $activePlugins as $key => $path )
-				{
-				
-					$searchFor = 'studiorum';
-
-					$pos = strpos( $path, $searchFor );
-
-					if( $pos === false ){
-						continue;
-					}
-
-					// Remove the path
-					$withoutPath = str_replace( $pathToRemove, '', $path );
-
-					// Now we just need the directory name
-					$splitPathAndSlug = explode( '/', $withoutPath );
-
-					// Search for 'studiorum' in the folder name
-					$folderName = strtolower( $splitPathAndSlug[0] );
-
-					$output[] = $folderName;
-
-				}
-
-				return $output;
-
-			}/* getActiveStudiorumPlugins() */
-
-
-			/**
-			 * Get all active plugins and strip off the path
-			 *
-			 * @since 0.1
-			 *
-			 * @param string $param description
-			 * @return string|int returnDescription
-			 */
-
-			public static function getAllActivePluginsWithoutPluginPath()
-			{
-
-				$activePlugins = wp_get_active_and_valid_plugins();
-
-				if( !$activePlugins || !is_array( $activePlugins ) || empty( $activePlugins ) ){
-					return;
-				}
-
-				$output = array();
-
-				// Each active plugin file has the plugin dir prepended, we need to strip that
-				$pathToRemove = WP_PLUGIN_DIR . '/';
-
-				foreach( $activePlugins as $key => $path )
-				{
-
-					// Remove the path
-					$withoutPath = str_replace( $pathToRemove, '', $path );
-
-					$output[] = $withoutPath;
-
-				}
-
-				return $output;
-
-			}/* getAllActivePluginsWithoutPluginPath() */
-
-
-			/**
-			 * Get the plugin 'groups' which are sets of plugins with some metadata about them
-			 *
-			 * @since 0.1
-			 *
-			 * @todo There should be a class stub which sets up each property and then each group should be an extension of that class which then adds a filter to a list. This method should then simply return that filter
-			 * @param null
-			 * @return array An array of plugin groups
-			 */
-
-			public static function getPluignGroups()
-			{
-
-				$groups = array(
-
-					'christina' => array(
-						'id'				=> 'christina',
-						'title'				=> 'Christina',
-						'icon' 				=> 'admin-appearance',
-						'excerpt' 			=> 'Enable your students to submit assignments, have them collaborate in groups and get inline feedback.',
-						'image' 			=> 'http://dummyimage.com/308x160',
-						'link' 				=> 'http://code.ubc.ca/studiorum/courses/christina',
-						'content' 			=> __( '<p>Create assignments - with deadlines - that enable your students to submit their work in a beautiful rich text editor. The student (and peers in their custom user groups) are able to make inline comments to get fine-grained critique.</p>', 'studiorum' ),
-						'content_sidebar' 	=> 'http://dummyimage.com/300x150',
-						'date'				=> '2014-06-01',
-						'plugins'			=> array(
-							'gravityforms/gravityforms.php',
-							'gravity-forms-custom-post-types/gfcptaddon.php',
-							'gravity-forms-wysiwyg/gf_wysiwyg.php',
-							'studiorum/studiorum.php',
-							'studiorum-lectio/studiorum-lectio.php',
-							'studiorum-side-comments/studiorum-side-comments.php',
-							'studiorum-user-groups/studiorum-user-groups.php'
-						),
-						'examples' 			=> array(
-							'http://arts.ubc.ca/arts-one/'
-						)
+				'christina' => array(
+					'id'				=> 'christina',
+					'title'				=> 'Christina',
+					'icon' 				=> 'admin-appearance',
+					'excerpt' 			=> 'Enable your students to submit assignments, have them collaborate in groups and get inline feedback.',
+					'image' 			=> 'http://dummyimage.com/308x160',
+					'link' 				=> 'http://code.ubc.ca/studiorum/courses/christina',
+					'content' 			=> __( '<p>Create assignments - with deadlines - that enable your students to submit their work in a beautiful rich text editor. The student (and peers in their custom user groups) are able to make inline comments to get fine-grained critique.</p>', 'studiorum' ),
+					'content_sidebar' 	=> 'http://dummyimage.com/300x150',
+					'date'				=> '2014-06-01',
+					'plugins'			=> array(
+						'gravityforms/gravityforms.php',
+						'gravity-forms-custom-post-types/gfcptaddon.php',
+						'gravity-forms-wysiwyg/gf_wysiwyg.php',
+						'studiorum/studiorum.php',
+						'studiorum-lectio/studiorum-lectio.php',
+						'studiorum-side-comments/studiorum-side-comments.php',
+						'studiorum-user-groups/studiorum-user-groups.php'
 					),
-					
-					'simon' => array(
-						'id'				=> 'simon',
-						'title'				=> 'Simon',
-						'icon' 				=> 'lightbulb',
-						'excerpt' 			=> 'Enable students to rate each others\' work simply and easily using a hot-or-not style voting system.',
-						'image' 			=> 'http://dummyimage.com/308x160/222/fff',
-						'link' 				=> 'http://code.ubc.ca/studiorum/courses/simon',
-						'content' 			=> __( '<p>Provide a simple way for your students to provide hot-or-not style feedback to easily discover which content is best.</p>', 'studiorum' ),
-						'content_sidebar' 	=> 'http://dummyimage.com/300x150',
-						'date'				=> '2014-07-01',
-						'plugins'			=> array(
-							'gravityforms/gravityforms.php',
-							'gravity-forms-custom-post-types/gfcptaddon.php',
-							'gravity-forms-wysiwyg/gf_wysiwyg.php',
-							'studiorum/studiorum.php',
-							'studiorum-lectio/studiorum-lectio.php',
-							'studiorum-hon/studiorum-hon.php'
-						),
-						'examples' 			=> array(
-							'http://physics.ubc.ca/'
-						)
-					),
-
-					'paul' => array(
-						'id'				=> 'paul',
-						'title'				=> 'Paul',
-						'icon' 				=> 'share',
-						'excerpt' 			=> 'Curate content via delicious.com and show it on your site in a beautiful, searchable, engaging way.',
-						'image' 			=> 'http://dummyimage.com/308x160/fff/333',
-						'link' 				=> 'http://code.ubc.ca/studiorum/courses/paul',
-						'content' 			=> __( '<p>Curate and tag content using the popular delicious.com service and then allow students to search quickly and easily to see the content you want them to see.</p>', 'studiorum' ),
-						'content_sidebar' 	=> 'http://dummyimage.com/300x150',
-						'date'				=> '2014-05-01',
-						'plugins'			=> array(
-							'studiorum/studiorum.php',
-							'studiorum-hon/studiorum-delicious.php'
-						),
-						'examples' 			=> array(
-							'http://sauder.ubc.ca/'
-						)
-					),
-
-				);
-
-				return $groups;
-
-			}/* getPluignGroups() */
-
-
-			/**
-			 * Helper method to determine if the passed plugin slug is an active plugin
-			 *
-			 * @since 0.1
-			 *
-			 * @param string $slug slug of plugin to check if it is active
-			 * @param array $plugins An array of plugins to check through
-			 * @return string|int returnDescription
-			 */
-
-			public static function isStudiorumPluginActive( $slug = false, $plugins = false )
-			{
-
-				if( !$slug ){
-					return false;
-				}
-
-				if( !$plugins ){
-					$plugins = static::getActiveStudiorumPlugins();
-				}
-
-				// Sanitize the slug
-				$slug = sanitize_text_field( $slug );
+					'examples' 			=> array(
+						'http://arts.ubc.ca/arts-one/'
+					)
+				),
 				
-				if( in_array( $slug, array_values( $plugins ) ) ){
-					return true;
-				}
+				'simon' => array(
+					'id'				=> 'simon',
+					'title'				=> 'Simon',
+					'icon' 				=> 'lightbulb',
+					'excerpt' 			=> 'Enable students to rate each others\' work simply and easily using a hot-or-not style voting system.',
+					'image' 			=> 'http://dummyimage.com/308x160/222/fff',
+					'link' 				=> 'http://code.ubc.ca/studiorum/courses/simon',
+					'content' 			=> __( '<p>Provide a simple way for your students to provide hot-or-not style feedback to easily discover which content is best.</p>', 'studiorum' ),
+					'content_sidebar' 	=> 'http://dummyimage.com/300x150',
+					'date'				=> '2014-07-01',
+					'plugins'			=> array(
+						'gravityforms/gravityforms.php',
+						'gravity-forms-custom-post-types/gfcptaddon.php',
+						'gravity-forms-wysiwyg/gf_wysiwyg.php',
+						'studiorum/studiorum.php',
+						'studiorum-lectio/studiorum-lectio.php',
+						'studiorum-hon/studiorum-hon.php'
+					),
+					'examples' 			=> array(
+						'http://physics.ubc.ca/'
+					)
+				),
 
+				'paul' => array(
+					'id'				=> 'paul',
+					'title'				=> 'Paul',
+					'icon' 				=> 'share',
+					'excerpt' 			=> 'Curate content via delicious.com and show it on your site in a beautiful, searchable, engaging way.',
+					'image' 			=> 'http://dummyimage.com/308x160/fff/333',
+					'link' 				=> 'http://code.ubc.ca/studiorum/courses/paul',
+					'content' 			=> __( '<p>Curate and tag content using the popular delicious.com service and then allow students to search quickly and easily to see the content you want them to see.</p>', 'studiorum' ),
+					'content_sidebar' 	=> 'http://dummyimage.com/300x150',
+					'date'				=> '2014-05-01',
+					'plugins'			=> array(
+						'studiorum/studiorum.php',
+						'studiorum-hon/studiorum-delicious.php'
+					),
+					'examples' 			=> array(
+						'http://sauder.ubc.ca/'
+					)
+				),
+
+			);
+
+			return $groups;
+
+		}/* getPluignGroups() */
+
+
+		/**
+		 * Helper method to determine if the passed plugin slug is an active plugin
+		 *
+		 * @since 0.1
+		 *
+		 * @param string $slug slug of plugin to check if it is active
+		 * @param array $plugins An array of plugins to check through
+		 * @return string|int returnDescription
+		 */
+
+		public static function isStudiorumPluginActive( $slug = false, $plugins = false )
+		{
+
+			if( !$slug ){
 				return false;
+			}
 
-			}/* isStudiorumPluginActive() */
+			if( !$plugins ){
+				$plugins = static::getActiveStudiorumPlugins();
+			}
 
-			/**
-			 * Determine whether a plugin is 'new' or not - basically has it been added in the last month
-			 *
-			 * @since 0.1
-			 *
-			 * @param string $pluginDate - the date the plugin was published
-			 * @return bool
-			 */
+			// Sanitize the slug
+			$slug = sanitize_text_field( $slug );
+			
+			if( in_array( $slug, array_values( $plugins ) ) ){
+				return true;
+			}
 
-			public static function isPluginNew( $pluginDate = false )
-			{
+			return false;
 
-				if( !$pluginDate ){
-					return false;
-				}
+		}/* isStudiorumPluginActive() */
 
-				$now = strtotime( 'now' );
-				$dateToCheck = strtotime( $pluginDate );
-				$thirtyDaysAgo = strtotime( '30 days ago' );
+		/**
+		 * Determine whether a plugin is 'new' or not - basically has it been added in the last month
+		 *
+		 * @since 0.1
+		 *
+		 * @param string $pluginDate - the date the plugin was published
+		 * @return bool
+		 */
 
-				if( $dateToCheck > $thirtyDaysAgo ){
-					return true;
-				}
+		public static function isPluginNew( $pluginDate = false )
+		{
 
+			if( !$pluginDate ){
 				return false;
+			}
 
-			}/* isPluginNew() */
+			$now = strtotime( 'now' );
+			$dateToCheck = strtotime( $pluginDate );
+			$thirtyDaysAgo = strtotime( '30 days ago' );
+
+			if( $dateToCheck > $thirtyDaysAgo ){
+				return true;
+			}
+
+			return false;
+
+		}/* isPluginNew() */
+
+		
 
 	}/* class Studiorum_Utils */
